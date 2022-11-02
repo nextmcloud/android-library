10113288cfa0f3ed44383db2b1f29d11babec6c3
tobiasKaminsky <tobias@kaminsky.me>
2022-11-02 11:50:24 +0100

    Run stable25 Dashboard only on NC25+
    Signed-off-by: tobiasKaminsky <tobias@kaminsky.me>

--- library/src/androidTest/java/com/nextcloud/android/lib/resources/dashboard/DashboardGetWidgetItemsRemoteOperationIT.kt
+++ library/src/androidTest/java/com/nextcloud/android/lib/resources/dashboard/DashboardGetWidgetItemsRemoteOperationIT.kt
@@ -27,21 +27,19 @@ import com.owncloud.android.lib.resources.files.CreateFolderRemoteOperation
 import com.owncloud.android.lib.resources.shares.CreateShareRemoteOperation
 import com.owncloud.android.lib.resources.shares.OCShare
 import com.owncloud.android.lib.resources.shares.ShareType
-import com.owncloud.android.lib.resources.status.GetCapabilitiesRemoteOperation
 import com.owncloud.android.lib.resources.status.NextcloudVersion
-import com.owncloud.android.lib.resources.status.OCCapability
 import org.junit.Assert.assertTrue
-import org.junit.Assume.assumeTrue
+import org.junit.Before
 import org.junit.Test
 
 class DashboardGetWidgetItemsRemoteOperationIT : AbstractIT() {
+    @Before
+    fun before() {
+        onlyOnMin(NextcloudVersion.nextcloud_25)
+    }
+
     @Test
     fun getItems() {
-        // only on NC25+
-        val ocCapability = GetCapabilitiesRemoteOperation()
-            .execute(nextcloudClient).singleData as OCCapability
-        assumeTrue(ocCapability.version.isNewerOrEqual(NextcloudVersion.nextcloud_25))
-
         // create folder to have some content
         assertTrue(CreateFolderRemoteOperation("/testFolder", false).execute(client2).isSuccess)
         assertTrue(
